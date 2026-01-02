25ecfae7ce6238ac3eb6471cbfa5b7df6f2cd2f4
Telami <telami.giuseppe@gmail.com>
2026-01-02 16:26:28 +0100

    fix: error when objective event unlock an empty bossroom

--- src/main/java/com/littlesquad/dungeon/internal/boss/BossRoomManager.java
+++ src/main/java/com/littlesquad/dungeon/internal/boss/BossRoomManager.java
@@ -1,12 +1,52 @@
 package com.littlesquad.dungeon.internal.boss;
 
+import com.littlesquad.dungeon.api.boss.Boss;
 import com.littlesquad.dungeon.api.boss.BossRoom;
+import com.littlesquad.dungeon.api.rewards.Reward;
+import org.bukkit.entity.Player;
 
+import java.util.Collections;
+import java.util.List;
 import java.util.Map;
+import java.util.UUID;
 import java.util.concurrent.ConcurrentHashMap;
 
 public final class BossRoomManager {
     private static final BossRoomManager instance = new BossRoomManager();
+
+    private static final BossRoom DUMMY = new BossRoom() {
+        public String getID () {
+            return "";
+        }
+        public boolean join (final UUID playerId) {
+            return false;
+        }
+        public void join (final Player... players) {}
+        public int capacity () {
+            return 0;
+        }
+        public boolean onePartyOnly () {
+            return false;
+        }
+        public List<String> accessDeniedCommands () {
+            return Collections.emptyList();
+        }
+        public BossRoom fallback () {
+            return null;
+        }
+        public List<String> enqueuingCommands () {
+            return Collections.emptyList();
+        }
+        public Boss getBoss () {
+            return null;
+        }
+        public List<Reward> rewards () {
+            return Collections.emptyList();
+        }
+        public Player[] getPlayersIn () {
+            return new Player[0];
+        }
+    };
 
     private final Map<String, BossRoom> bossRooms = new ConcurrentHashMap<>();
 
@@ -21,6 +61,8 @@ public final class BossRoomManager {
     }
 
     public BossRoom get (final String id) {
+        if (id.isEmpty())
+            return DUMMY;
         return bossRooms.get(id);
     }
 
